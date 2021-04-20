b0cb915e3feb99f5e4f8b01d0f472495709c5c8c
Nabin Katwal <nabeenkatwal7@gmail.com>
2021-04-20 09:22:28 +0545

    PDA cotd..

--- Lab2/main.cpp
+++ Lab2/main.cpp
@@ -2,23 +2,31 @@
 using namespace std;
 const int MAX = 100;
 
-void pda(char str[]){
-    cout<<str;
-}
-
 int stack(){
     int stack_init[MAX];
     int z = -1;
     int top = z;
     int header = z;
+
     return 0;
 }
 
+void pda(string str){
+    int state[MAX] = {0,1,2};
+//    int string_int[MAX];
+    bool flag = true;
+    for(int i=0;i<=str.length();i++){
+        if (str[i]=='1'){
+
+        }
+    }
+}
+
 int main() {
-    char str[MAX];
+    string str;
     cout<<"Enter string:"<<endl;
     fflush(stdin);
-    fgets(str, 100, stdin);
+    cin>>str;
 
     pda(str);
     return 0;
